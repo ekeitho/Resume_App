253220aa059811e710593825f2613422672c79df
Keith Abdulla <email@ekeitho.com>
2014-12-14 11:30:02 -0800

    goodbye button, hello swipe refresh

--- app/src/main/java/com/ekeitho/resume/github/GithubFragment.java
+++ app/src/main/java/com/ekeitho/resume/github/GithubFragment.java
@@ -1,17 +1,16 @@
 package com.ekeitho.resume.github;
 
+
+import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
-import android.util.AttributeSet;
-import android.util.Log;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.ekeitho.resume.R;
@@ -28,59 +27,70 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Keithmaynn on 12/13/14.
  */
-public class GithubFragment extends FragmentActivity {
+public class GithubFragment extends Fragment {
 
-    private Button refreshButton;
-    private ListView view;
-    private GithubAdapter githubAdapter;
+    private GithubAdapter mGithubAdapter;
+    private ListView mListView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
     private List<Repo> list = new ArrayList<Repo>();
+    private Activity mainActivity;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mainActivity = activity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.gitSwipeRefreshLayout);
+        mListView = (ListView) rootView.findViewById(R.id.github_repos);
 
 
-        this.setContentView(R.layout.fragment_main);
-        refreshButton = (Button) this.findViewById(R.id.refreshButton);
-        view = (ListView) this.findViewById(R.id.github_repos);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         // first set the adapter as empty
-        githubAdapter = new GithubAdapter(list, GithubFragment.this);
+        mGithubAdapter = new GithubAdapter(list, mainActivity.getApplicationContext());
         // set the views adapter to our created one
-        view.setAdapter(githubAdapter);
+        mListView.setAdapter(mGithubAdapter);
 
+        mSwipeRefreshLayout.setColorSchemeColors(R.color.material_blue_grey_800,
+                R.color.material_blue_grey_900,
+                R.color.material_blue_grey_800,
+                R.color.material_blue_grey_800);
 
-        refreshButton.setOnClickListener(new View.OnClickListener() {
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onClick(View v) {
+            public void onRefresh() {
                 new AsyncListViewLoader().execute("https://api.github.com/users/ekeitho/repos?sort=pushed");
             }
         });
-
     }
 
-
     public class AsyncListViewLoader extends AsyncTask<String, Void, List<Repo>> {
-
-        private final ProgressDialog dialog = new ProgressDialog(GithubFragment.this);
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            dialog.setMessage("Downloading contacts...");
-            dialog.show();
+
         }
 
         @Override
@@ -132,9 +142,11 @@ public class GithubFragment extends FragmentActivity {
         @Override
         protected void onPostExecute(List<Repo> result) {
             super.onPostExecute(result);
-            dialog.dismiss();
-            githubAdapter.setItemList(result);
-            githubAdapter.notifyDataSetChanged();
+            // notify the adapter on change
+            mGithubAdapter.setItemList(result);
+            mGithubAdapter.notifyDataSetChanged();
+            // set the refresh off
+            mSwipeRefreshLayout.setRefreshing(false);
         }
 
 
